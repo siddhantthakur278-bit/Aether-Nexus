8e45c68ab582e628e2fb60852f4b241b0534c7c7
siddhantthakur278-bit <siddhantthakur278@gmail.com>
2026-04-26 11:01:53 +0530

    feat(project2): establish MongoDB connection

--- project2/src/server.js
+++ project2/src/server.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
+const DB = process.env.MONGODB_URI;
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection successful!'))
+  .catch(err => console.error('DB connection error:', err));
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
